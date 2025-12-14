93e1856d0172843d9fc67f1330ece7551d28f706
Hivza <hivza.9@gmail.com>
2025-12-14 20:45:30 +0100

    Create a folder with a file in it

--- src/fs/mkpart_fat32.c
+++ src/fs/mkpart_fat32.c
@@ -24,8 +24,8 @@ typedef struct fat32_bios_parameter
 	// Extended boot record
 	uint32_t fat_sector_count;
 	uint16_t flags;
-	uint32_t fat_version;
-	uint32_t nb_cluster_root;
+	uint16_t fat_version;
+	uint32_t nb_root_cluster;
 	uint16_t fsinfo_sector;
 	uint16_t backup_boot_record_sector;
 	uint8_t reserved0[12];
@@ -36,7 +36,7 @@ typedef struct fat32_bios_parameter
 	char volumme_name[11];
 	char system_iditenfier[8];
 
-	uint8_t unused2[0x1a2];
+	uint8_t unused2[0x1a4];
 	uint16_t signature1;
 
 } __attribute__((packed)) fat32_bios_parameter_t;
@@ -52,6 +52,24 @@ typedef struct fat32_FSinfo
 	uint32_t signature3;
 
 } __attribute__((packed)) fat32_FSinfo_t;
+
+typedef struct DirEntry
+{
+	char Name[8];
+	char Extention[3];
+	uint8_t Attribute0;
+	uint8_t Attribute1;
+	uint8_t create_time_ms;
+	uint16_t create_time;
+	uint16_t create_date;
+	uint16_t access_date;
+	uint16_t cluster_nb_high;
+	uint16_t modified_time;
+	uint16_t modified_date;
+	uint16_t cluster_nb_low;
+	uint32_t size;
+
+} __attribute__((packed)) DirEntry_t;
 
 int main()
 {
@@ -99,7 +117,7 @@ int main()
 	fat32_bios_parameter.fat_sector_count = 0x0800;
 	fat32_bios_parameter.flags = 0x0000;
 	fat32_bios_parameter.fat_version = 0x0000;
-	fat32_bios_parameter.nb_cluster_root = 2;
+	fat32_bios_parameter.nb_root_cluster = 2;
 	fat32_bios_parameter.fsinfo_sector = 0x0001;
 	fat32_bios_parameter.backup_boot_record_sector = 0x0006;
 	fat32_bios_parameter.nb_drive = 0x80;
@@ -122,7 +140,6 @@ int main()
 	fat32_FSinfo.signature3 = 0xaa550000;
 
 	// write it
-	/*
 	fwrite(&fat32_bios_parameter, 512, 1, fptr); // 0
 	fwrite(&fat32_FSinfo, 512, 1, fptr);				 // 1
 	for (uint8_t i = 2; i < 6; i++)
@@ -135,7 +152,6 @@ int main()
 	{
 		fwrite(empty_sector, 512, 1, fptr);
 	}
-		*/
 
 	// fat
 	uint32_t FAT[fat32_bios_parameter.fat_sector_count * sector_size / sizeof(uint32_t)];
@@ -147,9 +163,82 @@ int main()
 	FAT[0] = 0x0ffffff8;
 	FAT[1] = 0x0fffffff;
 
+#define Cluster_size fat32_bios_parameter.sectors_per_cluster *sector_size
+
+#define READ_ONLY 0x01
+#define HIDDEN 0x02
+#define SYSTEM 0x04
+#define VOLUME_LABEL 0x08
+#define DIRECTORY 0x10
+
+	uint8_t Clusters[16][Cluster_size];
+	for (uint32_t i = 0; i < sizeof(Clusters); i++)
+	{
+		((uint8_t *)Clusters)[i] = 0;
+	}
+
+	DirEntry_t *root;
+	root = (DirEntry_t *)&Clusters[0];
+	FAT[2] = 0x0fffffff;
+
+	for (uint8_t i = 0; i < 11; i++)
+	{
+		root[0].Name[i] = "UNCERTAINTY"[i];
+	}
+	root[0].Attribute0 = VOLUME_LABEL;
+
+	for (uint8_t i = 0; i < 11; i++)
+	{
+		root[1].Name[i] = "FOLDER     "[i];
+	}
+	root[1].Attribute0 = DIRECTORY;
+	root[1].cluster_nb_low = 3;
+
+	DirEntry_t *folder;
+	folder = (DirEntry_t *)&Clusters[1];
+	FAT[3] = 0x0fffffff;
+
+	for (uint8_t i = 0; i < 11; i++)
+	{
+		folder[0].Name[i] = ".          "[i];
+	}
+	folder[0].Attribute0 = DIRECTORY;
+	folder[0].cluster_nb_low = 3;
+
+	for (uint8_t i = 0; i < 11; i++)
+	{
+		folder[1].Name[i] = "..         "[i];
+	}
+	folder[1].Attribute0 = DIRECTORY;
+
+	for (uint8_t i = 0; i < 8; i++)
+	{
+		folder[2].Name[i] = "FILENAME"[i];
+	}
+	for (uint8_t i = 0; i < 3; i++)
+	{
+		folder[2].Extention[i] = "TXT"[i];
+	}
+	folder[2].cluster_nb_low = 4;
+	folder[2].size = Cluster_size + 13;
+
+	FAT[4] = 5;
+	FAT[5] = 0x0fffffff;
+	for (uint32_t i = 0; i < Cluster_size; i++)
+	{
+		Clusters[2][i] = "Writing Data!\n"[i % 14];
+	}
+	for (uint32_t i = 0; i < 13; i++)
+	{
+		Clusters[3][i] = "\nNew Cluster!"[i];
+	}
+
 	// 2 copies are written
 	fwrite(FAT, sizeof(FAT), 1, fptr);
 	fwrite(FAT, sizeof(FAT), 1, fptr);
 
+	fwrite(Clusters, Cluster_size * 16, 1, fptr);
+
+	printf("Image builded successfully\n");
 	return (0);
 }
